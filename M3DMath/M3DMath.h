f2273bd755152d513420ecee26f78f9b6d417ee4
Old-Fritz <komarov.99@yandex.ru>
2019-05-03 13:06:40 +0300

    add new conertions and == operator

--- M3DMath/M3DMath.h
+++ M3DMath/M3DMath.h
@@ -3,6 +3,11 @@
 namespace M3DM
 {
 	/// Base vector structs
+	struct Vector2;
+	struct Vector3;
+	struct Vector4;
+
+
 	struct Vector2
 	{
 		float x, y;
@@ -11,8 +16,12 @@ namespace M3DM
 		Vector2() : x(0), y(0) {}
 		Vector2(const Vector2& vector) = default;
 		Vector2(float _x, float _y) : x(_x), y(_y) {}
-		explicit Vector2(const float* pArray) : x(pArray[0]), y(pArray[1]) {}
 		Vector2& operator=(const Vector2& vector) = default;
+
+		// Convertions
+		explicit Vector2(const float* pArray) : x(pArray[0]), y(pArray[1]) {}
+		explicit Vector2(const Vector3& vector);
+		explicit Vector2(const Vector4& vector);
 
 		// Base operations
 		Vector2 operator+(const Vector2& vector) const;
@@ -37,9 +46,13 @@ namespace M3DM
 		Vector3() : x(0), y(0), z(0) {}
 		Vector3(const Vector3& vector) = default;
 		Vector3(float _x, float _y, float _z) : x(_x), y(_y), z(_z) {}
+		Vector3& operator=(const Vector3& vector) = default;
+
+		// Convertions
 		explicit Vector3(const Vector2& vector) : x(vector.x), y(vector.y), z(0) {}
 		explicit Vector3(const float* pArray) : x(pArray[0]), y(pArray[1]), z(pArray[2]) {}
-		Vector3& operator=(const Vector3& vector) = default;
+		explicit Vector3(const Vector4& vector);
+
 
 		// Base operations
 		Vector3 operator+(const Vector3& vector) const;
@@ -64,22 +77,27 @@ namespace M3DM
 		Vector4() : x(0), y(0), z(0), w(0) {}
 		Vector4(const Vector4& vector) = default;
 		Vector4(float _x, float _y, float _z, float _w) : x(_x), y(_y), z(_z), w(_w) {}
+		Vector4& operator=(const Vector4& vector) = default;
+
+		// Convertions
 		explicit Vector4(const Vector2& vector) : x(vector.x), y(vector.y), z(0), w(0) {}
 		explicit Vector4(const Vector3& vector) : x(vector.x), y(vector.y), z(vector.z), w(0) {}
 		explicit Vector4(const float* pArray) : x(pArray[0]), y(pArray[1]), z(pArray[2]), w(pArray[3]) {}
-		Vector4& operator=(const Vector4& vector) = default;
 
 		// Base operations
 		Vector4 operator+(const Vector4& vector) const;
 		Vector4 operator-(const Vector4& vector) const;
 		Vector4 operator*(const Vector4& vector) const;
+		Vector4 operator/(const Vector4& vector) const;
 		Vector4 operator*(float scale) const;
 		Vector4 operator/(float scale) const;
 		Vector4& operator+=(const Vector4& vector);
 		Vector4& operator-=(const Vector4& vector);
 		Vector4& operator*=(const Vector4& vector);
+		Vector4& operator/=(const Vector4& vector);
 		Vector4& operator*=(float scale);
 		Vector4& operator/=(float scale);
+		bool operator==(const Vector4& vector);
 	};
 
 	/// Different operator combinations
