7b25c7220fca03361be532910f40a1d3cb7eec47
Old-Fritz <komarov.99@yandex.ru>
2019-05-03 12:35:26 +0300

    add / to header

--- M3DMath/M3DMath.h
+++ M3DMath/M3DMath.h
@@ -1,91 +1,80 @@
 #pragma once
 
-namespace M3DMath
+namespace M3DM
 {
-	/// Base structs
+	/// Base vector structs
 	struct Vector2
 	{
-		float x;
-		float y;
+		float x, y;
 
+		// Constructors
 		Vector2() : x(0), y(0) {}
+		Vector2(const Vector2& vector) = default;
 		Vector2(float _x, float _y) : x(_x), y(_y) {}
 		explicit Vector2(const float* pArray) : x(pArray[0]), y(pArray[1]) {}
-		Vector2& operator=(const Vector2& vector)
-		{
-			x = vector.x;
-			y = vector.y;
-			return *this;
-		}
+		Vector2& operator=(const Vector2& vector) = default;
 
-		Vector2 operator+(const Vector2& vector);
-		Vector2 operator-(const Vector2& vector);
-		Vector2 operator*(const Vector2& vector);
-		Vector2 operator*(float scale);
-		Vector2 operator/(float scale);
+		// Base operations
+		Vector2 operator+(const Vector2& vector) const;
+		Vector2 operator-(const Vector2& vector) const;
+		Vector2 operator*(const Vector2& vector) const;
+		Vector2 operator/(const Vector2& vector) const;
+		Vector2 operator*(float scale) const;
+		Vector2 operator/(float scale) const;
 		Vector2& operator+=(const Vector2& vector);
 		Vector2& operator-=(const Vector2& vector);
 		Vector2& operator*=(const Vector2& vector);
+		Vector2& operator/=(const Vector2& vector);
 		Vector2& operator*=(float scale);
 		Vector2& operator/=(float scale);
 	};
 
 	struct Vector3
 	{
-		float x;
-		float y;
-		float z;
+		float x, y, z;
 
+		// Constructors
 		Vector3() : x(0), y(0), z(0) {}
+		Vector3(const Vector3& vector) = default;
 		Vector3(float _x, float _y, float _z) : x(_x), y(_y), z(_z) {}
 		explicit Vector3(const Vector2& vector) : x(vector.x), y(vector.y), z(0) {}
 		explicit Vector3(const float* pArray) : x(pArray[0]), y(pArray[1]), z(pArray[2]) {}
-		Vector3& operator=(const Vector3& vector)
-		{
-			x = vector.x;
-			y = vector.y;
-			z = vector.z;
-			return *this;
-		}
+		Vector3& operator=(const Vector3& vector) = default;
 
-		Vector3 operator+(const Vector3& vector);
-		Vector3 operator-(const Vector3& vector);
-		Vector3 operator*(const Vector3& vector);
-		Vector3 operator*(float scale);
-		Vector3 operator/(float scale);
+		// Base operations
+		Vector3 operator+(const Vector3& vector) const;
+		Vector3 operator-(const Vector3& vector) const;
+		Vector3 operator*(const Vector3& vector) const;
+		Vector3 operator/(const Vector3& vector) const;
+		Vector3 operator*(float scale) const;
+		Vector3 operator/(float scale) const;
 		Vector3& operator+=(const Vector3& vector);
 		Vector3& operator-=(const Vector3& vector);
 		Vector3& operator*=(const Vector3& vector);
+		Vector3& operator/=(const Vector3& vector);
 		Vector3& operator*=(float scale);
 		Vector3& operator/=(float scale);
 	};
 
 	struct Vector4
 	{
-		float x;
-		float y;
-		float z;
-		float w;
+		float x, y, z, w;
 
+		// Constructors
 		Vector4() : x(0), y(0), z(0), w(0) {}
+		Vector4(const Vector4& vector) = default;
 		Vector4(float _x, float _y, float _z, float _w) : x(_x), y(_y), z(_z), w(_w) {}
 		explicit Vector4(const Vector2& vector) : x(vector.x), y(vector.y), z(0), w(0) {}
 		explicit Vector4(const Vector3& vector) : x(vector.x), y(vector.y), z(vector.z), w(0) {}
 		explicit Vector4(const float* pArray) : x(pArray[0]), y(pArray[1]), z(pArray[2]), w(pArray[3]) {}
-		Vector4& operator=(const Vector4& vector)
-		{
-			x = vector.x;
-			y = vector.y;
-			z = vector.z;
-			w = vector.w;
-			return *this;
-		}
+		Vector4& operator=(const Vector4& vector) = default;
 
-		Vector4 operator+(const Vector4& vector);
-		Vector4 operator-(const Vector4& vector);
-		Vector4 operator*(const Vector4& vector);
-		Vector4 operator*(float scale);
-		Vector4 operator/(float scale);
+		// Base operations
+		Vector4 operator+(const Vector4& vector) const;
+		Vector4 operator-(const Vector4& vector) const;
+		Vector4 operator*(const Vector4& vector) const;
+		Vector4 operator*(float scale) const;
+		Vector4 operator/(float scale) const;
 		Vector4& operator+=(const Vector4& vector);
 		Vector4& operator-=(const Vector4& vector);
 		Vector4& operator*=(const Vector4& vector);
@@ -93,27 +82,38 @@ namespace M3DMath
 		Vector4& operator/=(float scale);
 	};
 
-	// fifferent operator combinations
+	/// Different operator combinations
+	// Vector2 and Vector3
 	Vector3 operator*(const Vector3& vector1, const Vector2& vector2);
 	Vector3 operator*(const Vector2& vector1, const Vector3& vector2);
+	Vector3 operator/(const Vector3& vector1, const Vector2& vector2);
+	Vector3 operator/(const Vector2& vector1, const Vector3& vector2);
 	Vector3 operator+(const Vector3& vector1, const Vector2& vector2);
 	Vector3 operator+(const Vector2& vector1, const Vector3& vector2);
 	Vector3 operator-(const Vector3& vector1, const Vector2& vector2);
 	Vector3 operator-(const Vector2& vector1, const Vector3& vector2);
 
+	// Vector2 and Vector4
 	Vector4 operator*(const Vector4& vector1, const Vector2& vector2);
 	Vector4 operator*(const Vector2& vector1, const Vector4& vector2);
-	Vector4 operator*(const Vector4& vector1, const Vector3& vector2);
-	Vector4 operator*(const Vector3& vector1, const Vector4& vector2);
+	Vector4 operator/(const Vector4& vector1, const Vector2& vector2);
+	Vector4 operator/(const Vector2& vector1, const Vector4& vector2);
 	Vector4 operator+(const Vector4& vector1, const Vector2& vector2);
 	Vector4 operator+(const Vector2& vector1, const Vector4& vector2);
-	Vector4 operator+(const Vector4& vector1, const Vector3& vector2);
-	Vector4 operator+(const Vector3& vector1, const Vector4& vector2);
 	Vector4 operator-(const Vector4& vector1, const Vector2& vector2);
 	Vector4 operator-(const Vector2& vector1, const Vector4& vector2);
+
+	// Vector3 and Vector4
+	Vector4 operator*(const Vector4& vector1, const Vector3& vector2);
+	Vector4 operator*(const Vector3& vector1, const Vector4& vector2);
+	Vector4 operator/(const Vector4& vector1, const Vector3& vector2);
+	Vector4 operator/(const Vector3& vector1, const Vector4& vector2);
+	Vector4 operator+(const Vector4& vector1, const Vector3& vector2);
+	Vector4 operator+(const Vector3& vector1, const Vector4& vector2);
 	Vector4 operator-(const Vector4& vector1, const Vector3& vector2);
 	Vector4 operator-(const Vector3& vector1, const Vector4& vector2);
 
+	// Scalar operations
 	Vector2 operator*(float scale, const Vector2& vector);
 	Vector3 operator*(float scale, const Vector3& vector);
 	Vector4 operator*(float scale, const Vector4& vector);
